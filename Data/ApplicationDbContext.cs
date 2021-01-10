926dbfe4d0c95f5a0087480d680ea3560f470ab7
Daniel151505 <daniel150520011@gmail.com>
2021-01-09 19:06:52 -0500

    Agregando AplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibreriaProgra.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LibreriaProgra.Models.Producto> Productos { get; set; }
+        public DbSet<LibreriaProgra.Models.Usuario> Usuarios { get; set; }
+        public DbSet<LibreriaProgra.Models.OrdenDetail> OrdenDetails { get; set; }
+    }
+}
